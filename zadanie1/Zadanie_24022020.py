2dbcca4eb68fcbefafc7b0c9100aebd6d2fc2953
Nako-G <60852054+Nako-G@users.noreply.github.com>
2020-02-20 23:41:51 +0100

    Update Zadanie_24022020.py

--- zadanie1/Zadanie_24022020.py
+++ zadanie1/Zadanie_24022020.py
@@ -11,3 +11,16 @@ for i in range(len(lista2)):
         print("\n" "Wartość %s z listy1 spełnia wymagany warunek" %b)
     else:
         print("\n" "Ta pozycja z listy1 nie spełnia wymaganego warunku")
+        
+
+slownik1 = {"a": 1, "b": 2, "c": 3}
+slownik2 = {"a": 1, "c": 3, "d": 4}
+lista1 = ['a', 'b', 'x', 'd']
+print("slownik1=",slownik1)
+print("slownik1=",slownik2)
+print("lista1", lista1)
+print("\n")
+for i in lista1:
+   a = (slownik1.get(i,0))
+   b = (slownik2.get(i,0))
+   print("Wartość dla klucza %s: %s+%s=%s" %(i,a,b,a+b))
