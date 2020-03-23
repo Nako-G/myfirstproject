c1b5d2faa897e3b659b865ec3c9cb4f25c21d478
Nako-G <60852054+Nako-G@users.noreply.github.com>
2020-03-23 21:17:59 +0100

    Update Rozwiązanie_zadania_9.py

--- zadanie9/Rozwiązanie_zadania_9.py
+++ zadanie9/Rozwiązanie_zadania_9.py
@@ -17,3 +17,27 @@ if PotegaLiczby(int(float(n))):
     print('Ta liczba jest potęgą "2-ki"')
 else:
     print('Ta liczba nie jest potęgą "2-ki"')
+    
+ZADANIE_2
+#Napisz program ktory znajdzie najczesciej wystepujaca liczbe w podanej liscie.
+
+list = []
+liczby = int(input("Podaj ile liczb ma zawierać lista: "))
+for i in range(0, liczby):
+    liczby = int(input("Wpisz liczbę:"))
+    list.append(liczby)
+
+print("Twoja lista zawiera następujące liczby:", list)
+
+def najczesciej_wystepuje(list):
+    licznik = 0
+    element = list
+
+    for i in list:
+        najczesciej_wystepuje = list.count(i)
+        if (najczesciej_wystepuje > licznik):
+            licznik = najczesciej_wystepuje
+            element = i
+
+    return element
+print("Najczęściej występująca liczba w podanej liście to:",najczesciej_wystepuje(list))
