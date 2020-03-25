4e7720b60bea4504f154636055e0d4fb5570de30
Nako-G <60852054+Nako-G@users.noreply.github.com>
2020-03-25 20:22:28 +0100

    Update Rozwiązanie_zadania_9.py

--- zadanie9/Rozwiązanie_zadania_9.py
+++ zadanie9/Rozwiązanie_zadania_9.py
@@ -7,16 +7,16 @@ def PotegaLiczby(n):
     if n == 0 or n <0:
         return False
     while n != 0:
-        if n % 2 != 0: #dlaczego ten warunek nie chce działać dla float???
+        if n % 2 != 0: 
             return False
         n = n // 2
         return True
 
-
-if PotegaLiczby(int(float(n))):
+    if PotegaLiczby((float(n))):
     print('Ta liczba jest potęgą "2-ki"')
 else:
     print('Ta liczba nie jest potęgą "2-ki"')
+
     
 ZADANIE_2
 #Napisz program ktory znajdzie najczesciej wystepujaca liczbe w podanej liscie.
