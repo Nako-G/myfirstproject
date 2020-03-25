c15b771272174d4325d94cf4587efef13020ee61
Nako-G <60852054+Nako-G@users.noreply.github.com>
2020-03-25 22:09:58 +0100

    Create Rozwiązanie_zadanie_10.py

---
+++ zadanie10/Rozwiązanie_zadanie_10.py
@@ -0,0 +1,21 @@
+ZADANIE 1
+
+import random
+x = random.randrange(1, 10)
+
+print("Znajdź liczbę z przedziału od 1 do 10 w trzech ruchach i wygraj nagrodę!")
+próba = 2
+for próba in range(próba, -1, -1):
+    liczba = int(input("Wpisz liczbę: "))
+#    print("Pozostało {0} prób.".format(próba))
+
+    if liczba < x:
+        print("ZA MALA LICZBA, spróbuj jeszcze raz (pozostało {0} prób)".format(próba))
+    elif liczba > x:
+        print("ZA DUZA LICZBA, spróbuj jeszcze raz (pozostało {0} prób)".format(próba))
+    else:
+        if liczba == x:
+            print("Brawo, wygrałeś!")
+        break
+else:
+    print("Koniec gry, niestety przegrałeś :(")
